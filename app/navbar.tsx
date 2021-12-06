1d94377e736b670019212ae2a066956b511e7768
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-12-06 21:29:01 +0800

    Add navbar component

---
+++ app/navbar.tsx
@@ -0,0 +1,26 @@
+import {NavLink} from "remix";
+
+const linkActiveClassName = 'text-purple-900 hover:border-purple-900 border-b-2';
+const linkInactiveClassName = 'text-gray-800 hover:border-gray-800 hover:border-b-2';
+
+export default function NavBar() {
+  return (
+    <header>
+      <div>
+        <nav aria-label="Main navigation" className="px-20 py-10">
+          <ul className="flex justify-end">
+            <li>
+              <NavLink className={({isActive}) => isActive ? linkActiveClassName : linkInactiveClassName} to="/">home</NavLink>
+            </li>
+            {/* <li className="pl-10">
+              <NavLink className={({isActive}) => isActive ? linkActiveClassName : linkInactiveClassName} to="/my-work">my work</NavLink>
+            </li>
+            <li className="pl-10">
+              <NavLink className={({isActive}) => isActive ? linkActiveClassName : linkInactiveClassName} to="/contact-me">contact me</NavLink>
+            </li> */}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
