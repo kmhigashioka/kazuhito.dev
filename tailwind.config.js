3b91d8006189a1d4273e6ae3782b01aaed0f747d
Kazuhito Higashioka <kazuhito_higashioka@engagerocket.co>
2021-12-10 22:07:27 +0800

    Change to home page (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
     fontFamily: {
       'sans': ['Poppins'],
     },
+    fontSize: {
+      'base': '1rem',
+      'lg': '1.333rem',
+      'xl': '1.777rem',
+      '2xl': '2.369rem',
+      '3xl': '3.157rem',
+      '4xl': '4.209rem',
+    },
+    zIndex: {
+      'minus-1': -1
+    }
   },
   variants: {},
   plugins: []
