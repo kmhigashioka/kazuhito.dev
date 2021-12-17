9c2944fdeeb7d3fe408a38ebad0ef321abfa10cb
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-12-18 02:08:14 +0800

    Update meta

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,57 +1,10 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import { json } from "remix";
+import type { MetaFunction } from "remix";
 import pictureSrc from "../assets/picture.svg";
 
-type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
-};
-
-// Loaders provide data to components and are only ever called on the server, so
-// you can connect to a database or run any server side code you want right next
-// to the component that renders it.
-// https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = () => {
-  let data: IndexData = {
-    resources: [
-      {
-        name: "Remix Docs",
-        url: "https://remix.run/docs"
-      },
-      {
-        name: "React Router Docs",
-        url: "https://reactrouter.com/docs"
-      },
-      {
-        name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d"
-      }
-    ],
-    demos: [
-      {
-        to: "demos/actions",
-        name: "Actions"
-      },
-      {
-        to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading"
-      },
-      {
-        to: "demos/params",
-        name: "URL Params and Error Boundaries"
-      }
-    ]
-  };
-
-  // https://remix.run/api/remix#json
-  return json(data);
-};
-
-// https://remix.run/api/conventions#meta
 export let meta: MetaFunction = () => {
   return {
-    title: "Remix Starter",
-    description: "Welcome to remix!"
+    title: "Hi! 👋 I'm Kazu.",
+    description: "Building software for humans."
   };
 };
 
