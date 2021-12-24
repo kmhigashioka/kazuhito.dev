ef1e8b09c0199d4ac7ee4364a48c4afbf7e56321
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-12-24 11:52:08 +0800

    Add visual tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -20,3 +20,28 @@ test('can navigate to employer page', async ({ page }) => {
   await page.click('text=EngageRocket');
   expect(page.url()).toBe(currentEmployer);
 });
+
+const viewports = [
+  [320, 710],
+  [375, 710],
+  [425, 710],
+  [768, 710],
+  [1024, 947],
+  [1440, 1184],
+  [2560, 2030],
+];
+
+viewports.forEach(([width, height]) => {
+  test.describe('visual tests', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto(url);
+      await page.setViewportSize({ width, height });
+      await page.waitForLoadState('networkidle');
+    });
+
+    test(`visual test for viewport ${width}x${height}`, async ({ page, browserName }) => {
+      const path = `${browserName}-${width}x${height}-index_page.png`;
+      expect(await page.screenshot()).toMatchSnapshot(path, { threshold: 0.2 });
+    });
+  });
+});
